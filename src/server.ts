7fb35d32bc613e5b516c86428a7b03e2f65e50f2
Victor Gomes <victorgomesdacosta18@gmail.com>
2024-12-16 00:13:01 -0300

    feat: add app.db

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import fastify from "fastify";
+import { knex } from "./database";
 
 const app = fastify()
 
-app.get('/hello', () => {
-    return 'Hello world'
+app.get('/hello', async () => {
+    const tables = await knex('sqlite_schema').select('*')
+
+    return tables
 })
 
 app.listen({
     port: 3333,
-}).then(() => {
+})
+.then(() => {
     console.log('HTTP Server Running!')
 })
