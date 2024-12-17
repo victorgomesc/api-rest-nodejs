3fcbf42e0e28846b6e9eeff1307004f1a3f6bc3c
Victor Gomes <victorgomesdacosta18@gmail.com>
2024-12-17 16:39:51 -0300

    TEST: create path and file tests

---
+++ test/example.spec.ts
@@ -0,0 +1,8 @@
+import { expect, test } from 'vitest'
+
+test ('o usuaruio conssegue criar uma nova transação', () => {
+
+    const responseStatusCode = 201
+
+    expect(responseStatusCode).toEqual(201)
+})
