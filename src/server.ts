6077d9ae49ea8e35de4c940f4a6b839e9b9b836c
Victor Gomes <victorgomesdacosta18@gmail.com>
2024-12-17 01:36:42 -0300

    feat: add Fastify plugins

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,10 @@
 import fastify from "fastify";
-import { knex } from "./database";
-import crypto from "node:crypto";
 import { env } from "./env";
+import { transactionsRoutes } from "./routes/transactions";
 
 const app = fastify()
 
-app.get('/hello', async () => {
-    const transaction = await knex('transactions').insert({
-        id: crypto.randomUUID(),
-        title: 'Transação de teste',
-        ammount: 1000,
-    })
-    .returning('*')
-    return transaction
-})
+app.register(transactionsRoutes)
 
 app.listen({
     port: env.PORT,
