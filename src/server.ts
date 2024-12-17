2d127c7389123121823abfbeaef3409c083f4d1d
Victor Gomes <victorgomesdacosta18@gmail.com>
2024-12-17 04:38:15 -0300

    feat: create GET request and routers

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,9 @@ import { transactionsRoutes } from "./routes/transactions";
 
 const app = fastify()
 
-app.register(transactionsRoutes)
+app.register(transactionsRoutes, {
+    prefix: 'trasactions',
+})
 
 app.listen({
     port: env.PORT,
