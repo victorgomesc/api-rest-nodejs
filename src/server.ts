64cb7639bdb81348d76631913ba36751422e7679
Victor Gomes <victorgomesdacosta18@gmail.com>
2024-12-17 00:27:24 -0300

    feat: add validation

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
 import { knex } from "./database";
 import crypto from "node:crypto";
+import { env } from "./env";
 
 const app = fastify()
 
@@ -15,7 +16,7 @@ app.get('/hello', async () => {
 })
 
 app.listen({
-    port: 3333,
+    port: env.PORT,
 })
 .then(() => {
     console.log('HTTP Server Running!')
