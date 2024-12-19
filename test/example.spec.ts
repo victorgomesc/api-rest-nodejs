58d6b07de94c2a92e135eba28bdffbf8a77a67b2
Victor Gomes <victorgomesdacosta18@gmail.com>
2024-12-18 22:57:19 -0300

    TEST: im testing get router e get list

--- test/example.spec.ts
+++
@@ -1,8 +0,0 @@
-import { expect, test } from 'vitest'
-
-test ('o usuaruio conssegue criar uma nova transação', () => {
-
-    const responseStatusCode = 201
-
-    expect(responseStatusCode).toEqual(201)
-})
